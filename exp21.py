4046ba6e8a2675703152d17fba0eb6f7583ee4fd
amananil47 <134473742+amananil47@users.noreply.github.com>
2023-05-24 10:36:13 +0530

    Add files via upload

---
+++ exp21.py
@@ -0,0 +1,9 @@
+#To write a python program to find the sum of first 10 natural numbers#
+n=10
+sum=0
+while(n>0):
+    sum=sum+n
+    n=n-1
+print("the sum of first 10 natural numbers is:",sum)
+    
+    
